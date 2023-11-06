9499a83eb83d401e4c043681f82b2e36546d11fb
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-07 00:28:58 +0530

    Files stored into Topics.

---
+++ Topics/1-Variables.js
@@ -0,0 +1,16 @@
+const accountId = 1333099
+let accountEmail = "laxman@gmail.com"
+var accoutPassword = "12345"
+accountCity = "Haldwani"
+let accountState            // value is undefined.
+
+// accountId = 3 // const declared datatype can't be changed.
+
+accountEmail = "javascript@gmail.com"
+accountPassword = "234546"
+accountCity = "Internet"
+
+console.log(accountId);
+
+// To print multiple variables.
+console.table([accountId, accountEmail, accoutPassword, accountCity. accountState])
