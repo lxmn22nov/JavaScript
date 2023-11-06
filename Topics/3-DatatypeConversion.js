e6ea6667b5c6dbbd7ce274944820b85c8e50b881
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-07 01:16:52 +0530

    Added DatatypeConversion file.

---
+++ Topics/3-DatatypeConversion.js
@@ -0,0 +1,18 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)          // NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
