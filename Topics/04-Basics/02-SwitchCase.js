a755bf1f8bed7271d502b38e956671789174f54a
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-11 17:26:42 +0530

    Added Switch case file.

---
+++ Topics/04-Basics/02-SwitchCase.js
@@ -0,0 +1,53 @@
+// Syntax:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 11
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+        break;
+}
