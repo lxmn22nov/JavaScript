b20f7de2d298e2aaacece6838f5607094f4ac4ed
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-11 17:10:41 +0530

    Added Basics-04 file.

---
+++ Topics/04-Basics/01-ControlFlow.js
@@ -0,0 +1,59 @@
+// Control Flow.
+/* Comparioson operators.
+< : less than
+> : greater than
+<= : less than equal to
+>= : greater than equal to 
+== : equal to
+=== : equal to(typechecks)
+!= : is not equal to
+!== : 
+*/
+// if(condition) {                         // condition should be true, to execute scope.
+
+// }
+
+const temperature = 41
+
+// if(temperature < 49) {
+//     console.log(`Temperature is ${temperature} less than 49.`);
+// } else {
+//     console.log(`Temperature is greater than 49`);
+// }
+
+const score = 200
+
+// if (score> 100) {
+//     const power = "fly"
+//     console.log(`Usser Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);                     // not defined, out of scope.
+
+const balance = 1000
+
+// Implicit scope.
+// if(balance > 500) console.log("True.");
+
+// if (balance < 500) {
+//     console.log("Less Than 500.");
+// } else if (balance < 750) {
+//     console.log("Less than 750.");
+// } else if (balance < 900) {
+//     console.log("Less than 900.");
+// } else {
+//     console.log("Less than 1200.");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard) {
+//     console.log("Allow to buy course.");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log(`Logged in Sucessfully.`);
+// }
