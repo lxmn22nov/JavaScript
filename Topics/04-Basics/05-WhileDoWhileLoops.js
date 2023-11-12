f93757fc767a28285ac01e1602115ff4e1064b33
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2023-11-12 11:37:42 +0530

    Added Whiledowhile files.

---
+++ Topics/04-Basics/05-WhileDoWhileLoops.js
@@ -0,0 +1,29 @@
+// While and do while Loops.
+
+// while (condition) {
+    
+// }
+
+let index = 0
+// while (index <= 20) {
+//     console.log(`Value of index is ${index}.`);
+//     index += 2
+// }
+
+let myArr = ['Captain America', 'Thor', 'Black Widow']
+
+let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}.`);  
+//     arr += 1 
+// }
+// ==============================================================================================================
+
+// do while loop.
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}.`);
+    score++
+} while (score <= 10);
